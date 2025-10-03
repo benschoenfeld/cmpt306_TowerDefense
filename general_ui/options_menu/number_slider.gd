9880f51e75c358f0d9f0c01cd366893d343e430e
Max Guest (mtg651) <mtg651@mail.usask.ca>
2025-10-03 16:58:49 -0600

    Added logic to NumberSlider class.

--- general_ui/options_menu/number_slider.gd
+++ general_ui/options_menu/number_slider.gd
@@ -4,26 +4,30 @@ extends Control
 ## A slider that returns the value of the slider and displays it.
 
 ## The [float] value that is changed by the slider.
-var value: float = 100.0
+@export_range(0.0, 100.0, 1.0) var current_value: float = 100.0
 
-## The [HSlider] node that changes the [param value].
+## The [HSlider] node that changes the [param current_value].
 @onready var value_slider = $HBoxContainer/ValueSlider
 
-## A [Label] that shows the [param value]
+## A [Label] that shows the [param current_value]
 @onready var value_label = $HBoxContainer/Percentage
 
-## Sets the [param value] to a new float.
+func _ready() -> void:
+	value_slider.value = current_value
+
+## Sets the [param current_value] to a new float.
 func set_value(new_amount: float) -> void:
-	pass
+	current_value = new_amount
 
-## Returns a the [param value].
+## Returns a the [param current_value].
 func get_value() -> float:
-	return value
+	return current_value
 
-## Changes the [param value] when the slider is dropped.
-func _on_value_slider_drag_ended(value_changed: bool) -> void:
-	pass # Replace with function body.
-
-## Changes the displayed [param value] as the user drags the slide.
+## Changes the displayed [param current_value] as the user drags the slide.
 func _on_value_slider_drag_started() -> void:
-	pass # Replace with function body.
+	value_label.text = str(value_slider.value) + "%"
+
+## Changes the [param current_value] when slider value is changed.
+func _on_value_slider_value_changed(value: float) -> void:
+	current_value = value
+	value_label.text = str(current_value) + "%"
