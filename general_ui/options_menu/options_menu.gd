ef663a5518688c425156dcc2d00c0d3a5076e4b3
Max Guest (mtg651) <mtg651@mail.usask.ca>
2025-10-03 16:11:30 -0600

    Created a number slider scene for the sound sliders.

--- general_ui/options_menu/options_menu.gd
+++ general_ui/options_menu/options_menu.gd
@@ -1,9 +1,3 @@
 class_name OptionsMenu
 
 extends Control
-
-var main: MainMenu = MainMenu.new()
-var pause: PauseMenu = PauseMenu.new()
-func _ready() -> void:
-	main._on_options_button_pressed()
-	pause._on_resume_button_pressed()
