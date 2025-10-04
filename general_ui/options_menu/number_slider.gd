02e3ca0e3fbc58277454434ad6716d0a56b3681b
Max Guest (mtg651) <mtg651@mail.usask.ca>
2025-10-03 19:33:06 -0600

    Implemented logic for OptionsMenu.

--- general_ui/options_menu/number_slider.gd
+++ general_ui/options_menu/number_slider.gd
@@ -3,8 +3,10 @@ class_name NumberSlider
 extends Control
 ## A slider that returns the value of the slider and displays it.
 
+signal value_changed
+
 ## The [float] value that is changed by the slider.
-@export_range(0.0, 100.0, 1.0) var current_value: float = 100.0
+@export_range(0.0, 1.0, 0.001) var current_value: float = 1.0
 
 ## The [HSlider] node that changes the [param current_value].
 @onready var value_slider = $HBoxContainer/ValueSlider
@@ -12,8 +14,11 @@ extends Control
 ## A [Label] that shows the [param current_value]
 @onready var value_label = $HBoxContainer/Percentage
 
-func _ready() -> void:
-	value_slider.value = current_value
+## Updates the [param value_slider], [param current_value],and [param value_label]
+func value_match(new_amount: float) -> void:
+	value_slider.value = new_amount
+	set_value(new_amount)
+	_change_value_label()
 
 ## Sets the [param current_value] to a new float.
 func set_value(new_amount: float) -> void:
@@ -25,9 +30,14 @@ func get_value() -> float:
 
 ## Changes the displayed [param current_value] as the user drags the slide.
 func _on_value_slider_drag_started() -> void:
-	value_label.text = str(value_slider.value) + "%"
+	_change_value_label()
 
 ## Changes the [param current_value] when slider value is changed.
 func _on_value_slider_value_changed(value: float) -> void:
 	current_value = value
-	value_label.text = str(current_value) + "%"
+	_change_value_label()
+	emit_signal("value_changed", current_value)
+
+## A private function so that the [param value_label] can change.
+func _change_value_label() -> void:
+	value_label.text = str(current_value * 100) + "%"
--- general_ui/options_menu/options_menu.gd
+++ general_ui/options_menu/options_menu.gd
@@ -6,23 +6,42 @@ extends Control
 ## This game options menu allows for the changing of music and SFX volume.
 ##
 ## @tutorial: https://shaggydev.com/2023/05/22/volume-sliders/
-## tutorial used for how to access the audio bus systems and [method GlobalScope.linear_to_db]
+## tutorial used for how to access the audio bus systems and db to linear
+
+## Slider for the music.
+@onready var music_slider: NumberSlider = $MainContentContainer/VBoxContainer/MusicVolume
+
+## Slider for the sound effects.
+@onready var sfx_slider: NumberSlider = $MainContentContainer/VBoxContainer2/SFXVolume
 
 func _ready() -> void:
-	pass
+	_match_sliders_to_audio()
 
 ## Sets an [AudioServer] bus to a certain db based on given input.
-func set_audio_bus(bus_name: String) -> void:
-	pass
+func set_audio_bus_volume(bus_name: String, new_value: float) -> void:
+	var bus_index = AudioServer.get_bus_index(bus_name)
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(new_value))
+
+## A getter for an [AudioServer] bus db to linear
+func get_audio_bus_volume(bus_name: String) -> float:
+	var bus_index = AudioServer.get_bus_index(bus_name)
+	return db_to_linear(AudioServer.get_bus_volume_db(bus_index))
 
 ## Matches the visual on the sliders to match the [AudioServer] bus settings. 
 func _match_sliders_to_audio() -> void:
-	pass
+	var music_volume = get_audio_bus_volume("Music")
+	var sfx_volume = get_audio_bus_volume("SFX")
+	music_slider.value_match(music_volume)
+	sfx_slider.value_match(sfx_volume)
 
 ## Private function to change the audio bus settings for SFX.
-func _on_sfx_volume_value_changed() -> void:
-	pass # Replace with function body.
+func _on_sfx_volume_value_changed(new_value: float) -> void:
+	set_audio_bus_volume("SFX", new_value)
 
 ## Private function to change the audio bus settings for music.
-func _on_music_volume_value_changed() -> void:
-	pass # Replace with function body.
+func _on_music_volume_value_changed(new_value: float) -> void:
+	set_audio_bus_volume("Music", new_value)
+
+## Removes self from the scene
+func _on_exit_button_pressed() -> void:
+	queue_free()
