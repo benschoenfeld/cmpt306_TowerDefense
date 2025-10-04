e076e5044eb8bd1e9b87453a2dbcf58ee229a3df
Max Guest (mtg651) <mtg651@mail.usask.ca>
2025-10-03 18:32:38 -0600

    Made documentation and stubs for OptionsMenu class.

--- general_ui/options_menu/options_menu.gd
+++ general_ui/options_menu/options_menu.gd
@@ -1,3 +1,28 @@
 class_name OptionsMenu
 
 extends Control
+## The options menu allows the user to change game options
+##
+## This game options menu allows for the changing of music and SFX volume.
+##
+## @tutorial: https://shaggydev.com/2023/05/22/volume-sliders/
+## tutorial used for how to access the audio bus systems and [method GlobalScope.linear_to_db]
+
+func _ready() -> void:
+	pass
+
+## Sets an [AudioServer] bus to a certain db based on given input.
+func set_audio_bus(bus_name: String) -> void:
+	pass
+
+## Matches the visual on the sliders to match the [AudioServer] bus settings. 
+func _match_sliders_to_audio() -> void:
+	pass
+
+## Private function to change the audio bus settings for SFX.
+func _on_sfx_volume_value_changed() -> void:
+	pass # Replace with function body.
+
+## Private function to change the audio bus settings for music.
+func _on_music_volume_value_changed() -> void:
+	pass # Replace with function body.
