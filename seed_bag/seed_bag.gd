966f33a2260974c20f2da2c91f3ddc40f6caa82f
Max Guest (mtg651) <mtg651@mail.usask.ca>
2025-10-09 20:29:47 -0600

    Added stubs and documentation to the SeedBad class.

--- seed_bag/seed_bag.gd
+++ seed_bag/seed_bag.gd
@@ -1,3 +1,25 @@
 class_name SeedBag
 
 extends Control
+## Holds the crops that the player can plant.
+
+## Allows tools to see what seed is selected.
+signal selected_seed(seed_selection)
+
+const MAX_SEED_SLOTS: int = 9
+
+## The options for what crops can be planted.
+@export var seed_choices = []
+
+@export var seed_button_holder: GridContainer
+
+## The current seed selected.
+var current_seed_selected
+
+## Once a button is pressed send the selected crop.
+func send_seed_info():
+	pass
+
+## A private function to set up the buttons
+func _setup_seed_buttons():
+	pass
