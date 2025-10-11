12d0f6f92f75c15c4a234eb07d34a5d341e09937
Max Guest (mtg651) <mtg651@mail.usask.ca>
2025-10-10 19:25:01 -0600

    Added button to spawn in the pause menu.

--- game_manager/game_manager.gd
+++ game_manager/game_manager.gd
@@ -2,3 +2,14 @@ class_name GameManager
 
 extends Node
 ## This is the scene that puts together game machanics.
+
+##
+@export var pause_menu_scene: PackedScene
+
+##
+@export var pause_menu_layer: CanvasLayer
+
+##
+func _on_pause_button_pressed() -> void:
+	var pause_menu: PauseMenu = pause_menu_scene.instantiate()
+	pause_menu_layer.add_child(pause_menu)
