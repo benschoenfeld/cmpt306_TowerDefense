bdf1ef2ff0b344f6095b0851b43e755536d4b5c8
dylanfroh <dyf362@usask.ca>
2025-10-15 15:26:03 -0600

    updated UI

--- general_ui/hud/money_counter.gd
+++ general_ui/hud/money_counter.gd
@@ -5,3 +5,7 @@ var max_val = 99999
 
 func _on_hud_money_changed(money):
 	$Number.text = str(money)
+
+
+func _on_game_manager_money_changed(new_amount: int) -> void:
+	pass # Replace with function body.
