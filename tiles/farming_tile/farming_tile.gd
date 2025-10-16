cd03c234040935b3e91eda848f5d4fb02ebf7e3c
Max Guest (mtg651) <mtg651@mail.usask.ca>
2025-10-16 14:42:47 -0600

    Added to FarmingTile class to better handle with Crops class.

--- tiles/farming_tile/farming_tile.gd
+++ tiles/farming_tile/farming_tile.gd
@@ -6,14 +6,17 @@ extends BaseTile
 ## This is the main tile that the player will use their tools to remove grass,
 ## water the ground and plant crops.
 
+## The [Crop] scene that will be spawned in.
+@export var crop_scene: PackedScene
+
+## Holds where the [param crop_scene] will be spawned into this scene.
+@export var crop_holder: Node2D
+
 ## The rate of [param saturation] that will be lost every tick.
 @export var loss_rate_saturation: float = 0.1
 
 ## A [Sprite2D] node that represents the tile type.
 @export var tile_sprite: Sprite2D
-
-## A [Sprite2D] node that represents the crop type.
-@export var crop_sprite: Sprite2D
 
 ## The current stats of the tile. Max satuation and visual style.
 var current_stats: FarmingTileStats
@@ -22,7 +25,7 @@ var current_stats: FarmingTileStats
 var saturation: float = 0.0
 
 ## The type of [Crop] that is being planted onto this [FarmingTile].
-var current_crop # Make this have a crop scene once that is created.
+var current_crop: Crop
 
 ## A [FarmTileStats] to be used to show grass.
 @onready var grass_stats: FarmingTileStats = preload("res://tiles/farming_tile/resources/grass.tres")
@@ -52,21 +55,35 @@ func set_tile_stats(new_stats: FarmingTileStats) -> void:
 		tile_sprite.frame = current_stats.tile_type
 
 ## Returns the current [Crop] scene.
-func get_crop():
+func get_crop() -> Crop:
 	return current_crop
 
 ## Sets the current [Crop] scene to the new [Crop] scene.
-func set_crop(new_crop) -> void:
-	current_crop = new_crop
-	# Set the tile to be the crop texture or if there is no crop remove texture
-	if new_crop:
-		crop_sprite.texture = new_crop.crop_texture
+func set_crop(new_crop: CropResource) -> void:
+	# If the current crop is null then we need to add a new crop scene.
+	if current_crop == null:
+		var crop: Crop = crop_scene.instantiate()
+		crop.set_crop_resource(new_crop)
+		crop.set_parent_tile(self)
+		current_crop = crop
+		crop_holder.add_child(crop)
 	else:
-		crop_sprite.texture = null
+		current_crop.set_crop_resource(new_crop)
 
 ## Returns a [bool] based on if there is a crop or not.
 func has_crop() -> bool:
 	return current_crop != null
+
+## Removes the [Crop] visually and returns the [CropResource].
+## Either returns a [CropResource] or [null] if there is no crop.
+func harvest_crop() -> CropResource:
+	if current_crop and current_crop.is_grown:
+		var current_crop_resource: CropResource = current_crop.get_crop_resource()
+		current_crop = null
+		crop_holder.get_child(0).queue_free()
+		return current_crop_resource
+	else:
+		return null
 
 ## Returns the [param saturation] amount.
 func get_saturation() -> float:
@@ -91,7 +108,7 @@ func _process(delta: float) -> void:
 ## Lowers the saturation every tick.
 func _lower_saturation(delta: float) -> void:
 	# It can only go down if it is a wet dirt tile.
-	if current_stats.tile_type == FarmingTileStats.TileType.WET_DIRT:
+	if current_stats.get_tile_type() == FarmingTileStats.TileType.WET_DIRT:
 		# Makes sure that it can only go down to zero.
 		if !has_saturation():
 			set_saturation(0.0)
