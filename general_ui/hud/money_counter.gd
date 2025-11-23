d0ce8ed2c383a8c41ca859c18533488b66033b22
Max Guest (mtg651) <mtg651@mail.usask.ca>
2025-11-22 23:30:48 -0600

    Updated documentation in MoneyCounter class.

--- general_ui/hud/money_counter.gd
+++ general_ui/hud/money_counter.gd
@@ -6,9 +6,13 @@ extends Control
 ## A reference to the money [Label] for displaying the amount of money.
 @export var money_label: Label
 
+## The minimun amount that can be displayed. 
 var min_val = 0
+
+## The maximun amount that can be displayed.
 var max_val = 99999
 
+## Updates the [param money_label] with an [int] amount.
 func display_amount(new_amount: int) -> void:
 	var count: int = clamp(new_amount, min_val, max_val)
 	money_label.text = str(count)
