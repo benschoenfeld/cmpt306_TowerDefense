9247a9d04ce0adba6289d657cbef7bd239e97e3a
Max Guest (mtg651) <mtg651@mail.usask.ca>
2025-11-22 23:03:47 -0600

    Updated documentation in CropResource class.

--- crops/resources/crop_resource.gd
+++ crops/resources/crop_resource.gd
@@ -1,27 +1,27 @@
 extends Resource
 class_name CropResource
 
-# Name of the crop
+## Name of the crop.
 @export var crop_name: String
 
-# Time it takes until crop is harvestable
+## The amount of time it will take until crop is harvestable.
 @export var grow_time: float = 10.0
 
-# Provided spritesheet of all crop stages
+## Provided spritesheet of all crop stages.
 @export var sprite_sheet: Texture2D = preload("res://crops/assets/Crop_Spritesheet.png")
 
-# Determines what frame the sprite starts as
+## Determines what frame the sprite starts as.
 @export var start_coords: Vector2i = Vector2i(0,0)
 
-# How many frames until full growth
+## How many frames until full growth.
 @export var frames: int = 6
 
-# Value of the crop
+## Money value of the crop when harvested.
 @export var value: int
 
-# The grown frame for UI seedbag
+## The grown frame for the UI in the [Seedbag].
 @export var icon: Texture2D
 
-## Method to get the value from a crop instantiation
+## Method to get the value from a crop instantiation. Returns [param value].
 func get_value() -> int:
 	return value
