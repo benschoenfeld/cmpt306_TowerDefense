babbf8276f9bdd56642c3192ca207d6e38e71a22
Prithvi Singh Mann <qpd572@usask.ca>
2025-11-24 16:30:20 -0800

    Added functions to check if the towers are being placed too close to each, and buy/afford functions

--- defenses/tower.gd
+++ defenses/tower.gd
@@ -3,11 +3,16 @@ class_name Tower
 
 @export var tool_manager: ToolManager
 
+@export var game_manager: GameManager
+
 @export var required_tool_index: int
 
 @export var grid_size: Vector2 = Vector2(32, 32)
 
-#@export var tower_sound: AudioStreamPlayer2D  #TODO
+@export var min_distance: float = 64.0
+
+@export var tower_sound: AudioStreamPlayer2D  #TODO
+@export var deny_sound: AudioStreamPlayer2D   #TODO
 
 
 func _ready() -> void:
@@ -20,9 +25,7 @@ func _unhandled_input(event: InputEvent) -> void:
 	
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_RIGHT:
 		if tool_manager == null:
-			print("DEBUGG Tower: current_tool = ", tool_manager.get_current_tool(), "required = ", required_tool_index, "selected_tower = ", tool_manager.get_selected_tower() if tool_manager.has_method("get_selected_tower") else tool_manager.selected_tower)
-
-		
+			return
 		if not tool_manager.has_method("get_current_tool"):
 			return
 		if tool_manager.get_current_tool() != required_tool_index:
@@ -39,15 +42,22 @@ func _unhandled_input(event: InputEvent) -> void:
 			return
 		
 		#check money
-		#if not _can_afford(towerRes.cost):
-			#print("Not enough moeny for: ", towerRes.tower_name, " cost:", towerRes.cost)
-			#return
+		if not _can_afford_a_tower(towerRes.cost):
+			if deny_sound:
+				deny_sound.play()
+			print("Not enough moeny for: ", towerRes.tower_name, " cost:", towerRes.cost)
+			return
 			
 		var pos = get_global_mouse_position()
 		if grid_size.x > 0 and grid_size.y > 0:
 			pos = _snap_position(pos)
 			
-		
+		if _is_too_close(pos):
+			if deny_sound:
+				deny_sound.play()
+			print("Tower cannot be placed too close to another tower")
+			return
+			
 		if towerRes.tower_scene == null:
 			push_error("TowerResource has no individual scene assigned")
 			return
@@ -61,8 +71,13 @@ func _unhandled_input(event: InputEvent) -> void:
 		add_child(tower_instance)
 		print("Tower placed at ", pos)
 		
+		if not tower_instance.is_in_group("Towers"):
+			tower_instance.add_to_group("Towers")
 		# deduct money for the cost of building tower
-		#_spend_money(towerRes.cost)
+		_buy_tower(towerRes.cost)
+		
+		if tower_sound:
+			tower_sound.play()
 		
 		
 
@@ -71,6 +86,24 @@ func _unhandled_input(event: InputEvent) -> void:
 func _snap_position(pos: Vector2) -> Vector2:
 	return Vector2(round(pos.x / grid_size.x) * grid_size.x,
 					round(pos.y / grid_size.y) * grid_size.y)
+					
+func _can_afford_a_tower(cost: int) -> bool:
+	if game_manager and game_manager.has_method("get_money"):
+		return game_manager.get_money() >= cost
+	return true
+
+func _buy_tower(cost: int) -> void:
+	if game_manager and game_manager.has_method("add_money"):
+		game_manager.add_money(-cost)
+		return
+		
+func _is_too_close(pos: Vector2) -> bool:
+	for node in get_tree().get_nodes_in_group("Towers"):
+		if node is Node2D:
+			if node.position.distance_to(pos) < min_distance:
+				return true
+	return false
+		
 
 
 func _on_tool_manager_selected_tower_changed(towerResorce: TowerResource) -> void:
