03262e9a2ddb4968093a973467abfe56a702903a
Max Guest (mtg651) <mtg651@mail.usask.ca>
2025-12-01 15:34:10 -0600

    Changed how towers are placed to be more like how crops are planted.

--- defenses/base/base.gd
+++ defenses/base/base.gd
@@ -1,6 +1,13 @@
 extends BaseTile
 class_name BuildBase
 
+##
+@onready var tower_holder: Node2D = $TowerHolder
+
 func _ready() -> void:
 	add_to_group("TowerBases")
 	set_meta("occupied", false)
+
+##
+func set_tower(new_tower: TowerInstance):
+	tower_holder.add_child(new_tower)
