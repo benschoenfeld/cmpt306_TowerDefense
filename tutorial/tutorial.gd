8b5df9a844a85ecf9448a9e2f4d183c795f41c32
Max Guest (mtg651) <mtg651@mail.usask.ca>
2025-12-01 22:03:58 -0600

    Added stubs for the tutorial script.

--- tutorial/tutorial.gd
+++ tutorial/tutorial.gd
@@ -1,1 +1,13 @@
 extends Control
+
+## Goes back to the previous section of the tutorial.
+func _on_previous_button_pressed() -> void:
+	pass # Replace with function body.
+
+## Moves to the next section of the tutorial.
+func _on_next_button_pressed() -> void:
+	pass # Replace with function body.
+
+## Skips the tutorial.
+func _on_skip_button_pressed() -> void:
+	pass # Replace with function body.
