e9205a50af9ff458d96a8701b7c6add48ad87318
Max Guest (mtg651) <mtg651@mail.usask.ca>
2025-12-02 19:57:11 -0600

    Added documentation and fixed var names.

--- data_structures/priority_queue/priority_queue.gd
+++ data_structures/priority_queue/priority_queue.gd
@@ -1,67 +1,74 @@
 class_name PriorityQueue
 
 extends RefCounted
-##
+## Puts the largest number at the front of the queue.
 ##
 ## @tutorial: https://www.geeksforgeeks.org/dsa/priority-queue-set-1-introduction/
+## Converted the code to be used in godot.
 
+## Stored the number.
 var queue: Array = []
 
-# Returns index of parent
-func get_parent(index: int):
-	return int((index - 1) / 2)
+## Returns index of parent.
+func get_parent(index: int) -> int:
+	return int((index - 1) / 2.0)
 
-# Returns index of left child
-func get_left_child(index: int):
+## Returns index of left child.
+func get_left_child(index: int) -> int:
 	return 2 * index + 1
 
-# Returns index of right child
-func get_right_child(index: int):
+## Returns index of right child.
+func get_right_child(index: int) -> int:
 	return 2 * index + 2
 
-# Shift up to maintain max-heap property
-func shiftUp(index: int):
+## Shift up to maintain max-heap property.
+func shift_up(index: int) -> void:
 	while index > 0 and queue[get_parent(index)] < queue[index]:
 		var old_value = queue[get_parent(index)]
 		queue[get_parent(index)] = queue[index]
 		queue[index] = old_value
 		index = get_parent(index)
 
-# Shift down to maintain max-heap property
-func shiftDown(index: int):
+## Shift down to maintain max-heap property.
+func shift_down(index: int) -> void:
 	var size: int = queue.size()
-	var maxIndex = index
-	var l = get_left_child(index)
-	if l < size and queue[l] > queue[maxIndex]:
-		maxIndex = l
-	var r = get_right_child(index)
-	if r < size and queue[r] > queue[maxIndex]:
-		maxIndex = r
+	var max_index: int = index
+	var left_child: int = get_left_child(index)
+	
+	if left_child < size and queue[left_child] > queue[max_index]:
+		max_index = left_child
+	
+	var right_child: int = get_right_child(index)
+	
+	if right_child < size and queue[right_child] > queue[max_index]:
+		max_index = right_child
 
-	if index != maxIndex:
+	if index != max_index:
 		var old_value = queue[index]
-		queue[index] = queue[maxIndex]
-		queue[maxIndex] = old_value
-		shiftDown(maxIndex)
+		queue[index] = queue[max_index]
+		queue[max_index] = old_value
+		shift_down(max_index)
 
-# Insert a new element
-func insert(p):
-	queue.append(p)
-	shiftUp(len(queue) - 1)
+## Insert a new element.
+func insert(item) -> void:
+	queue.append(item)
+	shift_up(len(queue) - 1)
 
-# Extract element with maximum priority
+## Extract element with maximum priority.
+## Returns the max element.
 func pop():
 	var size = len(queue)
 	if size == 0:
-		return -1
+		return null
 	var result = queue[0]
+	# Put the number at the back to the front.
 	queue[0] = queue[size - 1]
-	queue.remove_at(0)
-	shiftDown(0)
+	queue.remove_at(queue.size()-1)
+	shift_down(0)
 	return result
 
-# Get current maximum element
-func getMax():
+## Return current maximum element.
+func get_max():
 	if queue.is_empty():
-		return -1
+		return null
 	return queue[0]
