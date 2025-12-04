571e1a6f64e3ac9f3f6c45970f2dd14e83706b44
dylanfroh <dyf362@usask.ca>
2025-12-04 16:40:41 -0600

    tower_instance.gd docs

--- defenses/towerInstance/tower_instance.gd
+++ defenses/towerInstance/tower_instance.gd
@@ -1,24 +1,53 @@
 extends Node2D
 class_name TowerInstance
 
+## Represents a placed tower, using data from a [TowerResource].
+##
+## A [TowerInstance] is the instance of a tower.
+## It has texture, stats, firing behavior, and range information
+## from a [TowerResource] and works with [TowerCombat] to handle
+## enemy detection and firing logic.
+
+## Path to turret sprite node to displays the tower.
 @export var turret_path : NodePath = NodePath("Turret/Sprite")
+
+## Path to the where bullets will spawn from.
 @export var marker_path : NodePath = NodePath("Turret/Marker")
+
+## Path to the [Area2D] used for range detection.
 @export var range_area_path: NodePath = NodePath("AreaRange")
+
+## Path to [TowerCombat] script that handles firing.
 @export var tower_combat_path: NodePath = NodePath("TowerCombat")
 
+## The collision shape of tower attack radius.
 @export var area: CollisionShape2D
 
+## The resource for tower stats, cost, textures, and firing settings.
 var tower_resource: TowerResource = null 
 
+## Sets the tower resource.
+##
+## @param towerRes The [TowerResource] containing the tower’s data.
 func _set_resource(towerRes: TowerResource) -> void:
 	tower_resource = towerRes
 
+## Applies a [TowerResource] to the tower instance, updating visuals, combat settings,
+## and the detection radius.
+##
+## Updates turret sprite texture, the detection radius based on resource,
+## configures the attached [TowerCombat] with damage, fire rate, bullet speed.
+## Places the tower into the "Towers" group if not assigned
+##
+## @param towerRes The [TowerResource] used to configure the tower.
 func apply_tower_resource(towerRes: TowerResource) -> void:
+	# If resource is null quit
 	if towerRes == null:
 		print("ToweriNstance.apply_tower_resource -> resource is NULL")
 		return
 	tower_resource = towerRes
 	
+	# Turret sprite setup
 	var turret_sprite = get_node(turret_path)
 	if not turret_sprite:
 		turret_sprite = get_node("Turret/Sprite")
@@ -33,10 +62,12 @@ func apply_tower_resource(towerRes: TowerResource) -> void:
 				turret_sprite.texture = towerRes.turret_texture
 			else:
 				print("TowerInstance: turrent_texture is not set in the resource -> Keeping default texture")
-
+	
+	# Range setup
 	area.shape = CircleShape2D.new()
 	area.shape.radius = towerRes.area_range
-
+	
+	# TowerCombat setup
 	var towerCombat = get_node(tower_combat_path)
 	if towerCombat:
 		towerCombat.set("damage", int(towerRes.damage))
@@ -47,11 +78,12 @@ func apply_tower_resource(towerRes: TowerResource) -> void:
 	else:
 		print("TowerInstance: towercombat path not found")
 	
+	# Check if tower is in appropriate group
 	if not is_in_group("Towers"):
 		add_to_group("Towers")
 		
 	queue_redraw()
 
-#
+## Draws visual representation of tower attack range in the editor.
 func _draw():
 	draw_circle(Vector2.ZERO, tower_resource.area_range, Color(1.0, 1.0, 1.0, 0.49), false, 2, true)
